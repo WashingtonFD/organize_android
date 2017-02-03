6d1aa49124811a048c38f9e9d7d7c78267dd7d75
marcelamelo <marcelaamelo1986@gmail.com>
2017-02-02 22:47:06 -0200

    criação e estruturação do projeto.

---
+++ organize/app/src/main/java/com/organize4event/organize/ui/activities/BaseActivity.java
@@ -0,0 +1,8 @@
+package com.organize4event.organize.ui.activities;
+
+/**
+ * Created by marcela on 02/02/17.
+ */
+
+public class BaseActivity  {
+}
---
+++ organize/app/src/main/java/com/organize4event/organize/ui/activities/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.organize4event.organize.ui.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.organize4event.organize.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+}
