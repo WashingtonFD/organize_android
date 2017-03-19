01776649bc947777ac260eb160736e4d50f6fda5
marcelamelobh <marcelaamelo1986@gmail.com>
2017-03-19 20:51:27 -0300

    implements layout login

--- organize/app/src/main/java/com/organize4event/organize/ui/activities/LoginActivity.java
+++ organize/app/src/main/java/com/organize4event/organize/ui/activities/LoginActivity.java
@@ -3,6 +3,8 @@ package com.organize4event.organize.ui.activities;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.organize4event.organize.R;
 import com.organize4event.organize.commons.AppApplication;
@@ -22,6 +24,8 @@ public class LoginActivity extends BaseActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
 
